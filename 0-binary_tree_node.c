f9403355389af727f5fc30776abb208d08a8bc2e
mdallen5393 <allen_matthew@outlook.com>
2023-04-07 10:59:01 -0500

    Task 0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,6 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	newNode->parent = parent;
 	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
 	return (newNode);
 }
