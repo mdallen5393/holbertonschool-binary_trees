cfebf14f42369163ff77b02cdcb2e5a9b8370c65
mdallen5393 <allen_matthew@outlook.com>
2023-04-09 16:39:30 -0500

    Task 0. Is BST

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -3,7 +3,7 @@
 
 /**
  * is_bst - function that checks if a binary tree is a valid Binary Search Tree
- * 
+ *
  * @tree: pointer to the root node of the tree to check
  * @min: minimum allowed value for the current node
  * @max: maximum allowed value for the current node
@@ -11,24 +11,24 @@
  */
 int is_bst(const binary_tree_t *tree, int min, int max)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    if (tree->n < min || tree->n > max)
-        return (0);
+	if (tree->n < min || tree->n > max)
+		return (0);
 
-    return (is_bst(tree->left, min, tree->n - 1) &&
-            is_bst(tree->right, tree->n + 1, max));
+	return (is_bst(tree->left, min, tree->n - 1) &&
+			is_bst(tree->right, tree->n + 1, max));
 }
 
 /**
  * binary_tree_is_bst - function that checks if a binary tree is a valid Binary
  * Search Tree.
- * 
+ *
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if tree is a valid BST, 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-    return (is_bst(tree, INT_MIN, INT_MAX));
+	return (is_bst(tree, INT_MIN, INT_MAX));
 }
