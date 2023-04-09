74ac9677090c2cfcc6b617e80951bb1b695de16c
mdallen5393 <allen_matthew@outlook.com>
2023-04-09 16:09:48 -0500

    Task 18. Uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,12 +1,17 @@
 #include "binary_trees.h"
+#include "10-binary_tree_depth.c"
+#include "17-binary_tree_sibling.c"
 
 /**
  * binary_tree_uncle - function that finds the uncle of a node in a binary tree.
- * 
+ *
  * @node: pointer to the node to find the uncle
  * Return: pointer to the uncle node, or NULL on failure
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (!node || binary_tree_depth(node) < 2)
+		return (NULL);
 
+	return (binary_tree_sibling(node->parent));
 }
