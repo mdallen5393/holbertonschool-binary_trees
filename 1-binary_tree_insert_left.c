e1379bef7791d99d7bce4f28b5710e088fccd8d8
mdallen5393 <allen_matthew@outlook.com>
2023-04-09 10:19:35 -0500

    Task 1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,19 +11,19 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *temp;
+	binary_tree_t *temp;
 
-    if (!parent)
-        return (NULL);
+	if (!parent)
+		return (NULL);
 
-    temp = parent->left;
+	temp = parent->left;
 
-    parent->left = binary_tree_node(parent, value);
-    if (!parent->left) /* malloc failed */
-        return (NULL);
+	parent->left = binary_tree_node(parent, value);
+	if (!parent->left) /* malloc failed */
+		return (NULL);
 
-    parent->left->left = temp;
-    if (temp)
-        temp->parent = parent->left;
-    return (parent->left);
+	parent->left->left = temp;
+	if (temp)
+		temp->parent = parent->left;
+	return (parent->left);
 }
