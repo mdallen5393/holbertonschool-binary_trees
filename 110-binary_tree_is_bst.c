2f11bc9fb12a307604e4555437f6df2c96c4ee45
mdallen5393 <allen_matthew@outlook.com>
2023-04-10 08:13:29 -0500

    Task 0. Is BST

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -12,10 +12,7 @@
 int is_bst(const binary_tree_t *tree, int min, int max)
 {
 	if (!tree)
-		return (0);
-
-	if (!tree->left && !tree->right) /* leaves are BST */
-		return (1);
+        return (0);
 
 	if (tree->n < min || tree->n > max)
 		return (0);
@@ -33,5 +30,9 @@ int is_bst(const binary_tree_t *tree, int min, int max)
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	return (is_bst(tree, INT_MIN, INT_MAX));
+    if (!tree)
+        return (0);
+    if (!tree->left && !tree->right)
+        return (1);
+    return (is_bst(tree, INT_MIN, INT_MAX));
 }
